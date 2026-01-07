33f02c4556c52d5d2c9973859dd46dcc017acb18
Daedalus02 <mattia.scantamburlo02@gmail.com>
2026-01-07 18:14:03 +0100

    added global launcher with delay

---
+++ ws_24_assignments/src/group_24_assignment_2/launch/global_launcher.launch.py
@@ -0,0 +1,84 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler, EmitEvent, LogInfo
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+import os
+import yaml
+
+def generate_launch_description():
+    pkg_name = 'group_24_assignment_2'
+    
+    assignment2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('ir_launch'),
+                'launch',
+                'assignment_2.launch.py'
+            ])
+        ])
+    )
+
+    apriltag_launch = IncludeLaunchDescription(
+        FrontendLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare(pkg_name),
+                'launch',
+                'launch_AprilTag_node.yml'
+            ])
+        )
+    )
+
+    moveit_config_pkg = "ir_movit_config"
+    moveit_config = MoveItConfigsBuilder("ir_gripper", package_name=moveit_config_pkg).to_moveit_configs()
+    planner_node = Node(
+        package=pkg_name,
+        executable="planner_action_server",
+        name="planner_action_server",
+        parameters=[
+            {"use_sim_time": True},
+            moveit_config.to_dict(),
+        ],
+        output="screen",
+        emulate_tty=True
+    )
+
+    gripper_node = Node(
+        package= pkg_name,
+        executable="gripper",
+        name="gripper",
+        parameters=[
+            {"use_sim_time": True},
+            moveit_config.to_dict(),
+        ],
+        output="screen",
+    )
+
+    coordinator_node = Node(
+        package=pkg_name,
+        executable="coordinator_action_client",
+        name="coordinator_action_client",
+        output="screen",
+        emulate_tty=True
+    )
+
+    delay_coordinator = TimerAction(
+        period=10.0,
+        actions=[coordinator_node]
+    )
+
+    # Create launch descriptor.
+    ld = LaunchDescription()
+    
+    ld.add_action(assignment2_launch)
+    ld.add_action(planner_node)
+    ld.add_action(apriltag_launch)
+    ld.add_action(gripper_node)
+    ld.add_action(delay_coordinator)
+
+    return ld
